252b5bccb82378f1161be56d929c741a8ea220cf
Andrew Reid <andrew.o.reid@gmail.com>
2014-05-22 16:33:45 +0100

    Add static content, and some basic initial styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# Returns the page title on a per page basis.
+	def full_title(name)
+		base_title = "The Board Room"
+		if name.empty?
+			base_title
+		else
+			"#{base_title} | #{name}"
+		end
+	end
 end
